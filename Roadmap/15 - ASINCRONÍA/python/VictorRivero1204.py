699a87c8129e5ad7971ec702bd25fad6f38e1bd2
VictorRivero1204 <victor.rivero1204@gmail.com>
2026-05-04 01:25:45 -0400

    #15 - Python

---
+++ Roadmap/15 - ASINCRONÍA/python/VictorRivero1204.py
@@ -0,0 +1,27 @@
+import datetime
+import asyncio
+
+"""
+Ejercicio
+"""
+
+
+async def task(name: str, duration: int):
+    print(
+        f"Tarea: {name}. Duración: {duration}s. Inicio: {datetime.datetime.now()}")
+    await asyncio.sleep(duration)
+    print(
+    f"Tarea: {name}. Fin: {datetime.datetime.now()}"
+    )
+
+asyncio.run(task("1", 2))
+
+"""
+Extra
+"""
+
+async def async_task():
+    await asyncio.gather(task("C", 3), task("B", 2), task("A", 1))
+    await task("D", 1)
+
+asyncio.run(async_task())
